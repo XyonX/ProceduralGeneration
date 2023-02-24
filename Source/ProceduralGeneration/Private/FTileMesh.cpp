893ac73af3b1b107f91e99cbe195b9b5475670b3
joydip chakraborty <33289572+XyonX@users.noreply.github.com>
2023-02-24 22:44:16 +0530

    SEPARATE FTileMesh  STRUCT

---
+++ Source/ProceduralGeneration/Private/FTileMesh.cpp
@@ -0,0 +1,14 @@
+﻿// Fill out your copyright notice in the Description page of Project Settings.
+
+
+#include "FTileMesh.h"
+#include "Components/InstancedStaticMeshComponent.h"
+
+void FTileMesh::Init(AActor* owneractor)
+{
+	Owner =owneractor;
+	InstancedMesh = NewObject<UInstancedStaticMeshComponent>(Owner);
+	InstancedMesh->AttachToComponent(Owner->GetRootComponent(), FAttachmentTransformRules::KeepRelativeTransform);
+	InstancedMesh->SetStaticMesh(TileMesh);
+	InstancedMesh->SetVisibility(true);
+}
---
+++ Source/ProceduralGeneration/Public/FTileMesh.h
@@ -0,0 +1,45 @@
+﻿// Fill out your copyright notice in the Description page of Project Settings.
+
+#pragma once
+
+#include "CoreMinimal.h"
+#include "UObject/Object.h"
+#include "GameplayTagContainer.h"
+#include "FTile.h"
+#include "FTileMesh.generated.h"
+
+
+// Must call Init Function From 
+USTRUCT(BlueprintType)
+struct FTileMesh
+{
+	GENERATED_BODY()
+	void Init(AActor* owneractor);
+
+	UPROPERTY(EditAnywhere,BlueprintReadWrite,Category="TileMesh")
+	UStaticMesh*TileMesh;
+	TArray<FTile*>OwnerTileList;
+	UPROPERTY(EditAnywhere,BlueprintReadWrite,Category="TileMesh")
+	FGameplayTag MeshTag ;
+	UPROPERTY(EditAnywhere,BlueprintReadWrite,Category="TileMesh")
+	FGameplayTagContainer ComaptileMeshTag_Left ;
+	UPROPERTY(EditAnywhere,BlueprintReadWrite,Category="TileMesh")
+	FGameplayTagContainer ComaptileMeshTag_Right ;
+	UPROPERTY(EditAnywhere,BlueprintReadWrite,Category="TileMesh")
+	FGameplayTagContainer ComaptileMeshTag_Up ;
+	UPROPERTY(EditAnywhere,BlueprintReadWrite,Category="TileMesh")
+	FGameplayTagContainer ComaptileMeshTag_Down ;
+	UPROPERTY(VisibleAnywhere,BlueprintReadWrite)
+	AActor*Owner;
+	UPROPERTY(EditAnywhere,BlueprintReadWrite,Category="TileMesh")
+	UInstancedStaticMeshComponent*InstancedMesh;
+	
+};
+
+
+
+UCLASS()
+class PROCEDURALGENERATION_API UFTileMesh : public UObject
+{
+	GENERATED_BODY()
+};
