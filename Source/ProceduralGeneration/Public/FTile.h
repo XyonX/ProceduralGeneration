297728b66e6b240b316cf7abef0257ab077532a0
joydip chakraborty <33289572+XyonX@users.noreply.github.com>
2023-02-24 22:44:04 +0530

    SEPARATE FTile  STRUCT

---
+++ Source/ProceduralGeneration/Public/FTile.h
@@ -0,0 +1,65 @@
+﻿// Fill out your copyright notice in the Description page of Project Settings.
+
+#pragma once
+
+#include "CoreMinimal.h"
+#include "UObject/Object.h"
+#include "FTileMesh.h"
+#include "FTile.generated.h"
+
+
+//Collapse status enum
+UENUM(BlueprintType)
+enum class EcollapseStatus : uint8
+{
+	NotCollapsed = 0		UMETA(DisplayName = "Not Collapsed"),
+	Collapsed =1		UMETA(DisplayName = "Collaped")	
+};
+
+
+// 2D STRUCT FOR STORING XY LOCATION
+USTRUCT(BlueprintType)
+struct FMatrixPosition
+{
+	GENERATED_BODY()
+	
+	FMatrixPosition(int height , int width) :Height(height),Width(width){}
+	UPROPERTY(EditAnywhere,BlueprintReadWrite,Category="Position")
+	int Height ;
+	UPROPERTY(EditAnywhere,BlueprintReadWrite,Category="Position")
+	int Width ;
+	FORCEINLINE FMatrixPosition GetPosition () { FMatrixPosition pos(Height,Width); return pos;}
+};
+
+
+USTRUCT(BlueprintType)
+struct FTile
+{
+	GENERATED_BODY()
+
+	FTile();
+	FTile(int id ,FMatrixPosition pos2d ,FVector worldloc  , TArray<FTileMesh>& totaltilemesh); 
+
+	UPROPERTY(EditAnywhere,BlueprintReadWrite,Category="Tile")
+	int ID ;
+	//UPROPERTY(EditInstanceOnly,Category="Tile")
+	FTileMesh* SelectedTiledMesh;
+	UPROPERTY(EditAnywhere,BlueprintReadWrite,Category="Tile")
+	FMatrixPosition Position_2D ; 
+	UPROPERTY(EditAnywhere,BlueprintReadWrite,Category="Tile")
+	FVector World_Location ;
+	UPROPERTY(EditAnywhere,BlueprintReadWrite,Category="Tile")
+	EcollapseStatus CollapseStatus ;
+	void SetCollapseStatus( EcollapseStatus CollapseStatuss);
+	//UPROPERTY(BlueprintReadWrite,Category="Tile")
+	TArray<FTileMesh* > AllAvailableMeshToChooseFrom;
+	
+	
+	
+};
+
+UCLASS()
+class PROCEDURALGENERATION_API UFTile : public UObject
+{
+	GENERATED_BODY()
+};
