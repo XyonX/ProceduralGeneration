2fcb723557e8ee123ca8d9266d3734a12cc3ed84
joydip chakraborty <33289572+XyonX@users.noreply.github.com>
2023-02-27 00:46:43 +0530

    New TileMesh Class

---
+++ Source/ProceduralGeneration/Private/TileMesh.cpp
@@ -0,0 +1,32 @@
+﻿// Fill out your copyright notice in the Description page of Project Settings.
+
+
+#include "TileMesh.h"
+
+#include "Tile.h"
+#include "Components/InstancedStaticMeshComponent.h"
+
+
+UTileMesh::UTileMesh()
+{
+	
+}
+
+UTileMesh::~UTileMesh()
+{
+	
+	for (int32 i = OwnerTileList.Num() - 1; i >= 0; i--)
+	{
+		OwnerTileList.RemoveAt(i);
+	}
+	delete InstancedMesh ;
+}
+
+void UTileMesh::Init(AActor* owneractor)
+{
+	Owner =owneractor;
+	InstancedMesh =  NewObject<UInstancedStaticMeshComponent>(owneractor);
+	InstancedMesh->AttachToComponent(Owner->GetRootComponent(), FAttachmentTransformRules::KeepRelativeTransform);
+	InstancedMesh->SetStaticMesh(TileMesh);
+	InstancedMesh->SetVisibility(true);
+}
---
+++ Source/ProceduralGeneration/Public/TileMesh.h
@@ -0,0 +1,45 @@
+﻿// Fill out your copyright notice in the Description page of Project Settings.
+
+#pragma once
+
+#include "CoreMinimal.h"
+#include "UObject/Object.h"
+#include "GameplayTagContainer.h"
+#include "TileMesh.generated.h"
+
+
+
+class UTile ;
+
+UCLASS()
+class PROCEDURALGENERATION_API UTileMesh : public UObject
+{
+	GENERATED_BODY()
+
+public:
+	
+	UTileMesh();
+	~UTileMesh();
+	
+	void Init(AActor* owneractor);
+
+	UPROPERTY(EditAnywhere,BlueprintReadWrite,Category="TileMesh")
+	UStaticMesh*TileMesh;
+	UPROPERTY(EditAnywhere,BlueprintReadWrite,Category="TileMesh")
+	TArray<UTile*>OwnerTileList;
+	UPROPERTY(EditAnywhere,BlueprintReadWrite,Category="TileMesh")
+	FGameplayTag MeshTag ;
+	UPROPERTY(EditAnywhere,BlueprintReadWrite,Category="TileMesh")
+	FGameplayTagContainer ComaptileMeshTag_Left ;
+	UPROPERTY(EditAnywhere,BlueprintReadWrite,Category="TileMesh")
+	FGameplayTagContainer ComaptileMeshTag_Right ;
+	UPROPERTY(EditAnywhere,BlueprintReadWrite,Category="TileMesh")
+	FGameplayTagContainer ComaptileMeshTag_Up ;
+	UPROPERTY(EditAnywhere,BlueprintReadWrite,Category="TileMesh")
+	FGameplayTagContainer ComaptileMeshTag_Down ;
+	UPROPERTY(VisibleAnywhere,BlueprintReadWrite)
+	AActor*Owner;
+	UPROPERTY(EditAnywhere,BlueprintReadWrite,Category="TileMesh")
+	UInstancedStaticMeshComponent*InstancedMesh;
+	
+};
