1693e4ba2ca19d968eff2bacfc3d94f965110c1b
joydip chakraborty <33289572+XyonX@users.noreply.github.com>
2023-03-16 15:05:10 +0530

    Added Editor ToolKit

---
+++ Source/ProceduralGenerationEditor/DockTab/ProceduralEditorTab.cpp
@@ -0,0 +1,43 @@
+﻿// Fill out your copyright notice in the Description page of Project Settings.
+
+
+#include "ProceduralEditorTab.h"
+
+void SProceduralEditorTab::Construct(const FArguments& InArgs)
+{
+	// Call the parent constructor
+	SDockTab::Construct(
+		SDockTab::FArguments()
+			.TabRole(ETabRole::NomadTab)
+			.Label(FText::FromString("Procedural Generation"))
+	);
+
+	// This creates the procedural generation layout that will be added to the tab widget
+	TSharedRef<SHorizontalBox> ProceduralGenerationLayout = SNew(SHorizontalBox);
+
+	// Add a text block to the layout to display a label
+	ProceduralGenerationLayout->AddSlot()
+		.VAlign(VAlign_Center)
+		.HAlign(HAlign_Left)
+		.Padding(5.0f)
+		[SNew(STextBlock)
+			.Text(FText::FromString("Name:"))];
+
+	// Add a button to the layout to generate procedural content
+	ProceduralGenerationLayout->AddSlot()
+		.VAlign(VAlign_Center)
+		.HAlign(HAlign_Right)
+		.Padding(5.0f)
+		[SNew(SButton)
+			.Text(FText::FromString("Generate"))
+			.OnClicked(this, &SProceduralEditorTab::GenerateProceduralContent)];
+
+	// Set the content of the tab widget to be the procedural generation layout
+	SetContent(ProceduralGenerationLayout);
+}
+
+FReply SProceduralEditorTab::GenerateProceduralContent()
+{
+	// TODO: Implement procedural content generation logic here
+	return FReply::Handled();
+}
---
+++ Source/ProceduralGenerationEditor/DockTab/ProceduralEditorTab.h
@@ -0,0 +1,18 @@
+﻿#pragma once
+
+#include "Widgets/Docking/SDockTab.h"
+#include "Widgets/Layout/SScrollBox.h"
+
+class SProceduralEditorTab : public SDockTab
+{
+public:
+	SLATE_BEGIN_ARGS(SProceduralEditorTab)
+	{}
+
+	SLATE_END_ARGS()
+
+	void Construct(const FArguments& InArgs);
+
+private:
+	FReply GenerateProceduralContent();
+};
