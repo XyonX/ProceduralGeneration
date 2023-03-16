06747961bcfb9f43338098a8e0436f5b9d34a70b
joydip chakraborty <33289572+XyonX@users.noreply.github.com>
2023-03-16 10:11:28 +0530

    Added Main Slate UI For  Procedural Dock Tab ( No  Design JUST TEMPLATE )

---
+++ Source/ProceduralGenerationEditor/UI/SMainUI.cpp
@@ -0,0 +1,51 @@
+﻿// Fill out your copyright notice in the Description page of Project Settings.
+
+
+#include "SMainUI.h"
+#include "SlateOptMacros.h"
+
+BEGIN_SLATE_FUNCTION_BUILD_OPTIMIZATION
+FonButtonClick SMainUI::ButtonClickDelegate;
+void SMainUI::Construct(const FArguments& InArgs)
+{
+	ButtonClickDelegate.BindSP(this, &SMainUI::OnButtonClicked);
+
+	ChildSlot
+		[
+			SNew(SVerticalBox)
+			+ SVerticalBox::Slot()
+			.AutoHeight()
+			[
+				SNew(STextBlock)
+				.Text(FText::FromString(TEXT("My Custom Panel")))
+			]
+			+ SVerticalBox::Slot()
+			.AutoHeight()
+			[
+				SNew(SButton)
+				.Text(FText::FromString(TEXT("Button")))
+				//.OnClicked(this, &SMyCustomPanelWidget::HandleButtonClick)
+				.OnClicked_Static(&HandleButtonClick)
+			]
+		];
+}
+
+FReply SMainUI::HandleButtonClick()
+{
+	if(ButtonClickDelegate.IsBound() )
+	{
+		bool bHandled = ButtonClickDelegate.Execute();
+		if (bHandled)
+		{
+			return FReply::Handled();
+		}
+	}
+	return FReply::Unhandled();
+}
+
+bool SMainUI::OnButtonClicked()
+{
+	return  true;
+}
+
+END_SLATE_FUNCTION_BUILD_OPTIMIZATION
