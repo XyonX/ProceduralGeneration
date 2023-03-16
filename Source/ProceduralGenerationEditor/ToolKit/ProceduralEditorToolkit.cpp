f28bd79851bb1b53844779c38ed889d8459abc68
joydip chakraborty <33289572+XyonX@users.noreply.github.com>
2023-03-16 10:12:30 +0530

    Added ToolKit

---
+++ Source/ProceduralGenerationEditor/ToolKit/ProceduralEditorToolkit.cpp
@@ -0,0 +1,44 @@
+﻿// Fill out your copyright notice in the Description page of Project Settings.
+
+
+#include "ProceduralEditorToolkit.h"
+
+FProceduralEditorToolkit::FProceduralEditorToolkit()
+{
+}
+
+FProceduralEditorToolkit::~FProceduralEditorToolkit()
+{
+}
+
+void FProceduralEditorToolkit::Initialize(UObject* InAsset, const EToolkitMode::Type InMode,
+	const TSharedPtr<IToolkitHost>& InToolkitHost)
+{
+}
+
+void FProceduralEditorToolkit::ExtendToolbar()
+{
+}
+
+
+
+FName FProceduralEditorToolkit::GetToolkitFName() const
+{
+	return DefaultName;
+}
+
+FText FProceduralEditorToolkit::GetBaseToolkitName() const
+{
+	return DefaultText;
+}
+
+UObject* FProceduralEditorToolkit::GetEditingObject()
+{
+	return nullptr;
+}
+
+
+void FProceduralEditorToolkit::SaveAsset_Execute()
+{
+	FAssetEditorToolkit::SaveAsset_Execute();
+}
---
+++ Source/ProceduralGenerationEditor/ToolKit/ProceduralEditorToolkit.h
@@ -0,0 +1,40 @@
+﻿// Fill out your copyright notice in the Description page of Project Settings
+#pragma once
+
+#include "CoreMinimal.h"
+#include "ToolKits/AssetEditorToolkit.h"
+
+class FProceduralEditorToolkit : public FAssetEditorToolkit
+{
+public:
+	// Constructor
+	FProceduralEditorToolkit();
+
+	// Destructor
+	virtual ~FProceduralEditorToolkit();
+
+	// Initialize the editor toolkit
+	virtual void Initialize(UObject* InAsset, const EToolkitMode::Type InMode, const TSharedPtr<class IToolkitHost>& InToolkitHost) ;
+	
+
+	// Extend the toolbar with additional functionality
+	virtual void ExtendToolbar();
+
+	// Create the main tab layout of the editor toolkit
+	//virtual void CreateToolkitTabs(TSharedPtr<class FTabManager> TabManager) ;
+
+	// Get the name of the editor toolkit
+	virtual FName GetToolkitFName() const override;
+
+	// Get the label of the editor toolkit
+	virtual FText GetBaseToolkitName() const override;
+
+	// Get the object being edited by this editor toolkit
+	virtual UObject* GetEditingObject()    ;
+
+	// Save the asset being edited by this editor toolkit
+	virtual void SaveAsset_Execute() override;
+
+	FName DefaultName ;
+	FText DefaultText ;
+};
