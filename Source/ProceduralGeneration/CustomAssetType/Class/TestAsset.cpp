112069fb97261d6d3de8706f7bfa32f5a605853a
joydip chakraborty <33289572+XyonX@users.noreply.github.com>
2023-03-19 01:24:53 +0530

    Added Custom Asset  class

---
+++ Source/ProceduralGeneration/CustomAssetType/Class/TestAsset.cpp
@@ -0,0 +1,33 @@
+﻿// Fill out your copyright notice in the Description page of Project Settings.
+
+
+#include "TestAsset.h"
+
+
+// Sets default values
+ATestAsset::ATestAsset()
+{
+	// Set this actor to call Tick() every frame.  You can turn this off to improve performance if you don't need it.
+	PrimaryActorTick.bCanEverTick = true;
+}
+
+// Called when the game starts or when spawned
+void ATestAsset::BeginPlay()
+{
+	Super::BeginPlay();
+	
+}
+
+// Called every frame
+void ATestAsset::Tick(float DeltaTime)
+{
+	Super::Tick(DeltaTime);
+}
+
+void ATestAsset::Serialize(FArchive& Ar)
+{
+	Super::Serialize(Ar);
+	Ar<< TestInt;
+	
+}
+
---
+++ Source/ProceduralGeneration/CustomAssetType/Class/TestAsset.h
@@ -0,0 +1,35 @@
+﻿// Fill out your copyright notice in the Description page of Project Settings.
+
+#pragma once
+
+#include "CoreMinimal.h"
+#include "GameFramework/Actor.h"
+#include "TestAsset.generated.h"
+
+UCLASS()
+class PROCEDURALGENERATION_API ATestAsset : public AActor
+{
+	GENERATED_BODY()
+
+public:
+	// Sets default values for this actor's properties
+	ATestAsset();
+
+protected:
+	// Called when the game starts or when spawned
+	virtual void BeginPlay() override;
+
+public:
+	// Called every frame
+	virtual void Tick(float DeltaTime) override;
+
+	virtual void Serialize(FArchive& Ar) override;
+	virtual void GetLifetimeReplicatedProps(TArray<FLifetimeProperty>& OutLifetimeProps) const override;
+
+	UPROPERTY(SaveGame,EditAnywhere,BlueprintReadWrite,Category="Asset")
+	int  TestInt  ;
+	UPROPERTY(SaveGame,EditAnywhere,BlueprintReadWrite,Category="Asset")
+	int TestFloat;
+	UPROPERTY(SaveGame,EditAnywhere,BlueprintReadWrite,Category="Asset")
+	TArray<int>TestIntArray;
+};
