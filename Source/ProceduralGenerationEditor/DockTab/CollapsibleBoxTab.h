f531c7b68c720b4c6bfebe87835b0fadf8adb1a8
joydip chakraborty <33289572+XyonX@users.noreply.github.com>
2023-03-18 11:18:56 +0530

    Added New Docktab Wrapper  for the  CollapsibleBox

---
+++ Source/ProceduralGenerationEditor/DockTab/CollapsibleBoxTab.h
@@ -0,0 +1,36 @@
+﻿#pragma once
+
+#include "Widgets/Docking/SDockTab.h"
+#include "Widgets/Layout/SScrollBox.h"
+
+class SCollapsibleBox_01;
+
+class SCollapsibleBoxTab : public SDockTab
+{
+public:
+	SLATE_BEGIN_ARGS(SCollapsibleBoxTab)
+	{}
+
+	SLATE_END_ARGS()
+
+	SCollapsibleBoxTab();
+	void Construct(const FArguments& InArgs);
+
+	// Register the dockable tab
+	static void RegisterTabSpawner();
+
+	// Create an instance of the dockable tab
+	static TSharedRef<SDockTab> SpawnTab(const FSpawnTabArgs& SpawnTabArgs);
+
+
+	//THIS NAME IS FOR  IDENTIFY THIS ACTS   AS A  IDENTIFIER
+	static const FName TabName;
+	// Get the tab identifier
+	FName GetTabIdentifier() const;
+
+	
+
+private:
+	FReply GenerateProceduralContent();
+	TSharedPtr<SCollapsibleBox_01>MyWidget ;
+};
