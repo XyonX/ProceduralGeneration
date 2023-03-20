d6d53470b35d0dccf2f3c3ae980b74610db2b451
joydip chakraborty <33289572+XyonX@users.noreply.github.com>
2023-03-20 20:11:27 +0530

    Removed Custom TEST ASSET RELATED CLASSES AND CODE

--- Source/ProceduralGenerationEditor/ProceduralGenerationEditor.h
+++ Source/ProceduralGenerationEditor/ProceduralGenerationEditor.h
@@ -5,64 +5,12 @@
 #include "Modules/ModuleManager.h"
 
 class FMyEditorModule
-	: public IHasMenuExtensibility
-	, public IHasToolBarExtensibility
-	, public IModuleInterface
+	: public IModuleInterface
 {
 public:
 	virtual void StartupModule() override;
 	virtual void ShutdownModule() override;
-	virtual bool SupportsDynamicReloading() override;
 
-	
-	
-
-public:	//INTERFACE  OVERIDES
-	
-	//~ IHasMenuExtensibility interface
-	virtual TSharedPtr<FExtensibilityManager> GetMenuExtensibilityManager() override;
-	//~ IHasToolBarExtensibility interface
-	virtual TSharedPtr<FExtensibilityManager> GetToolBarExtensibilityManager() override;
-
-
-protected: //FUNCTION FOR CUSTOM ASSET  TYPE
-
-	/** Registers asset tool actions. */
-	void RegisterAssetTools();
-	/** Unregisters asset tool actions. */
-	void UnregisterAssetTools();
-	
-	void RegisterSettings();
-	/** Unregister the text asset editor settings. */
-	void UnregisterSettings();
-
-	void RegisterAssetTypeAction(IAssetTools& AssetTools, const TSharedRef<IAssetTypeActions>& Action);
-
-	void RegisterAssetFaccory();
-
-	/** Registers main menu and tool bar menu extensions. */
-	void RegisterMenuExtensions();
-	/** Unregisters main menu and tool bar menu extensions. */
-	void UnregisterMenuExtensions();
-
-	void ConfigureDirectory (const IAssetTools& AssetTools);
-
-	
-
-private:
-	
-	/** Holds the menu extensibility manager. */
-	TSharedPtr<FExtensibilityManager> MenuExtensibilityManager;
-
-	/** The collection of registered asset type actions. */
-	TArray<TSharedRef<IAssetTypeActions>> RegisteredAssetTypeActions;
-
-	/** Holds the plug-ins style set. */
-	TSharedPtr<ISlateStyle> Style;
-
-	/** Holds the tool bar extensibility manager. */
-	TSharedPtr<FExtensibilityManager> ToolBarExtensibilityManager;
-	
 	
 	
 };
