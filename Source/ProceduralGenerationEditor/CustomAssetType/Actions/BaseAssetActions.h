bd039eb598bca9c7d2866409f09cfaf33ff63ca6
joydip chakraborty <33289572+XyonX@users.noreply.github.com>
2023-03-21 14:33:07 +0530

    Added Base Actor Action

---
+++ Source/ProceduralGenerationEditor/CustomAssetType/Actions/BaseAssetActions.h
@@ -0,0 +1,20 @@
+﻿#pragma once
+
+#include "AssetTypeActions_Base.h"
+#include "ProceduralGeneration/Actors/CustomAssetClass/BaseActor.h"
+
+
+class FBaseActorActions : public FAssetTypeActions_Base
+{
+	
+public:
+	
+	virtual FText GetName() const override {return FText::FromString("Procedural Actor"); };
+	virtual uint32 GetCategories() override {return EAssetTypeCategories::Misc; };
+	virtual UClass* GetSupportedClass() const override {return UBaseActor::StaticClass();};
+	virtual bool HasActions(const TArray<UObject*>& InObjects) const override {return false;};
+	virtual void OpenAssetEditor(const TArray<UObject*>& InObjects, TSharedPtr<IToolkitHost> EditWithinLevelEditor) override{};
+	virtual bool CanFilter() override{return  true;};
+	virtual FColor GetTypeColor() const override{return FColor::Yellow;};
+	
+};
