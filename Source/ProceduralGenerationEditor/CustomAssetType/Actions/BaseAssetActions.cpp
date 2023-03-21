c3ea26b7d04d545d47c9ac7f605d54c97bfb2ade
joydip chakraborty <33289572+XyonX@users.noreply.github.com>
2023-03-22 00:00:28 +0530

    Updated Action class
    - Context menu working
    -Thumb slate is setting properly
    -image is not showing

--- Source/ProceduralGenerationEditor/CustomAssetType/Actions/BaseAssetActions.cpp
+++ Source/ProceduralGenerationEditor/CustomAssetType/Actions/BaseAssetActions.cpp
@@ -3,3 +3,107 @@
 
 #include "BaseAssetActions.h"
 
+#include "EditorStyleSet.h"
+#include "Brushes/SlateImageBrush.h"
+#include "Styling/SlateStyleRegistry.h"
+//#include "AssetThumbnail.h"
+
+
+#define LOCTEXT_NAMESPACE "BaseActorActions"
+FTestDelegate FBaseActorActions::TestDelegate;
+
+
+FBaseActorActions::FBaseActorActions()
+{
+	TestDelegate.BindRaw(this, &FBaseActorActions::AnotherFunction);
+}
+
+//}
+void FBaseActorActions::GetActions(const TArray<UObject*>& InObjects, FMenuBuilder& MenuBuilder)
+{
+
+	FAssetTypeActions_Base::GetActions(InObjects, MenuBuilder);
+
+	auto TextAssets = GetTypedWeakObjectPtrs<UBaseActor>(InObjects);
+	
+	MenuBuilder.AddMenuEntry(
+		FText::FromString("My Entry"),
+		FText::FromString("My Tooltip"),
+				FSlateIcon(),
+				FUIAction(
+			FExecuteAction::CreateStatic(&FBaseActorActions::TestFunction),
+			EUIActionRepeatMode::RepeatDisabled
+		));
+		
+
+}
+
+TSharedPtr<SWidget> FBaseActorActions::GetThumbnailOverlay(const FAssetData& AssetData) const
+{
+	TSharedPtr<SImage> ThumbnailImage;
+	UObject* Asset = AssetData.GetAsset();
+
+
+/*	// Load the custom image brush
+	const FName BrushName = FName(TEXT("/ProceduralGeneration/EditorData_UAsset/SB_BAThumb.SB_BAThumb"));
+	const FVector2D IconSize(64.f, 64.f);
+	const FSlateBrush* Brush = FAppStyle::GetBrush(BrushName);
+	if (Brush == nullptr)
+	{
+		UE_LOG(LogTemp, Error, TEXT("Failed to load custom thumbnail brush."));
+		return nullptr;
+	}*/
+	const FString ThumbnailPath = "/ProceduralGeneration/Source/ProceduralGenerationEditor/EditorContent/BaseActorThumbnail.jpg"; // Replace with your own image file path
+	
+	if (Asset)
+	{
+		ThumbnailImage = SNew(SImage)
+			.ColorAndOpacity(FLinearColor::Green)
+			.ToolTipText(FText::FromString("Thumbnail Tooltip"));
+	}
+	if (FPaths::FileExists(ThumbnailPath))
+	{
+		const FSlateBrush* ThumbnailBrush = FAppStyle::GetBrush("NoTexture");
+		FSlateImageBrush* ImageBrush = new FSlateImageBrush(ThumbnailPath, FVector2D(64.0f, 64.0f));
+		if (ImageBrush->GetResourceObject() == nullptr)
+		{
+			delete ImageBrush;
+			ImageBrush = nullptr;
+		}
+		else
+		{
+			ThumbnailBrush = ImageBrush;
+		}
+		ThumbnailImage->SetImage(ImageBrush);
+	}
+    
+	return SNew(SBox)
+		.WidthOverride(64.0f)
+		.HeightOverride(64.0f)
+		[
+			ThumbnailImage.ToSharedRef()
+		];
+}
+
+void FBaseActorActions::TestFunction()
+{
+	TestDelegate.Execute();
+	if (GEngine)
+	{
+		GEngine->AddOnScreenDebugMessage(-1,5.0f,FColor::Blue,FString("Context MenU Function Called "),false);
+	}
+	
+}
+
+bool FBaseActorActions::AnotherFunction()
+{
+	
+	if (GEngine)
+	{
+		GEngine->AddOnScreenDebugMessage(-1,5.0f,FColor::Blue,FString("CAnother Function Called "),false);
+	}
+	return true;
+}
+
+
+#undef LOCTEXT_NAMESPACE
--- Source/ProceduralGenerationEditor/CustomAssetType/Actions/BaseAssetActions.h
+++ Source/ProceduralGenerationEditor/CustomAssetType/Actions/BaseAssetActions.h
@@ -3,18 +3,28 @@
 #include "AssetTypeActions_Base.h"
 #include "ProceduralGeneration/Actors/CustomAssetClass/BaseActor.h"
 
+DECLARE_DELEGATE_RetVal(bool , FTestDelegate)
 
 class FBaseActorActions : public FAssetTypeActions_Base
 {
 	
 public:
+	FBaseActorActions();
 	
 	virtual FText GetName() const override {return FText::FromString("Procedural Actor"); };
 	virtual uint32 GetCategories() override {return EAssetTypeCategories::Misc; };
 	virtual UClass* GetSupportedClass() const override {return UBaseActor::StaticClass();};
-	virtual bool HasActions(const TArray<UObject*>& InObjects) const override {return false;};
+	virtual bool HasActions(const TArray<UObject*>& InObjects) const override {return true;};
+	virtual void GetActions(const TArray<UObject*>& InObjects, FMenuBuilder& MenuBuilder) override;
 	virtual void OpenAssetEditor(const TArray<UObject*>& InObjects, TSharedPtr<IToolkitHost> EditWithinLevelEditor) override{};
 	virtual bool CanFilter() override{return  true;};
 	virtual FColor GetTypeColor() const override{return FColor::Yellow;};
+	virtual TSharedPtr<SWidget> GetThumbnailOverlay(const FAssetData& AssetData) const override;
+
+	static FTestDelegate TestDelegate;
+	
+	static void TestFunction ();
+
+	bool AnotherFunction ();
 	
 };
