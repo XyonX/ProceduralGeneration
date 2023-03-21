340d81d98408e2d7603e73b1890d3dc5cf65929b
joydip chakraborty <33289572+XyonX@users.noreply.github.com>
2023-03-21 12:10:08 +0530

    Added  Base Actor Factory

---
+++ Source/ProceduralGenerationEditor/CustomAssetType/AssetFactory/BaseActorFactory.cpp
@@ -0,0 +1,19 @@
+﻿// Fill out your copyright notice in the Description page of Project Settings.
+
+
+#include "BaseActorFactory.h"
+#include "ProceduralGeneration/Actors/CustomAssetClass/BaseActor.h"
+
+UBaseActorFactory::UBaseActorFactory()
+{
+	bCreateNew = true;
+	bEditAfterNew = true;
+	SupportedClass = UBaseActor::StaticClass();
+}
+
+UObject* UBaseActorFactory::FactoryCreateNew(UClass* InClass, UObject* InParent, FName InName, EObjectFlags Flags,
+                                             UObject* Context, FFeedbackContext* Warn)
+{
+	UBaseActor* NewAsset = NewObject<UBaseActor>(InParent, InClass, InName, Flags);
+	return NewAsset;
+}
---
+++ Source/ProceduralGenerationEditor/CustomAssetType/AssetFactory/BaseActorFactory.h
@@ -0,0 +1,19 @@
+﻿#pragma once
+
+#include "CoreMinimal.h"
+#include "UObject/ObjectMacros.h"
+#include "Factories/Factory.h"
+#include "BaseActorFactory.generated.h"
+
+UCLASS()
+class UBaseActorFactory : public UFactory
+{
+	GENERATED_BODY()
+
+public:
+	UBaseActorFactory();
+
+	virtual UObject* FactoryCreateNew(UClass* InClass, UObject* InParent, FName InName, EObjectFlags Flags, UObject* Context, FFeedbackContext* Warn) override;
+
+	
+};
