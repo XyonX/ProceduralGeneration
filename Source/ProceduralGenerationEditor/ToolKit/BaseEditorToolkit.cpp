012d309e748ba04f9cda3a6658bf4ccb5d07ca80
joydip chakraborty <33289572+XyonX@users.noreply.github.com>
2023-03-22 13:03:54 +0530

    Added Toolkit class for the main editor

---
+++ Source/ProceduralGenerationEditor/ToolKit/BaseEditorToolkit.cpp
@@ -0,0 +1,55 @@
+﻿// Fill out your copyright notice in the Description page of Project Settings.
+
+
+#include "BaseEditorToolkit.h"
+
+#define LOCTEXT_NAMESPACE "Editor Tab"
+
+void FBaseEditorToolkit::Init()
+{
+	// Create the viewport client
+	ViewportClient = MakeShareable(new FViewportClient());
+
+	// Create the property view
+	//PropertyView = PropertyEditorModule.CreateDetailView(DetailsViewArgs);
+
+	// Create the command list
+	ToolkitCommands = MakeShareable(new FUICommandList);
+
+	// Set up the UI
+	const TSharedRef<FTabManager::FLayout> StandaloneDefaultLayout = FTabManager::NewLayout("Standalone_CustomTextureEditor_Layout_v1")
+		->AddArea
+		(
+			FTabManager::NewPrimaryArea()->SetOrientation(Orient_Horizontal)
+			->Split
+			(
+				FTabManager::NewStack()->SetSizeCoefficient(0.66f)
+				->AddTab("Viewport", ETabState::OpenedTab, FTabManager::FTab()
+					//.AddLabel(LOCTEXT("ViewportTabTitle", "Viewport"))
+					.SetContent
+					(
+						SNew(SViewport)
+							.ViewportInterface(ViewportClient.ToSharedRef())
+					)
+				)
+				->Split
+				(
+					FTabManager::NewStack()->SetSizeCoefficient(0.33f)
+					->AddTab("Details", ETabState::OpenedTab, FTabManager::FTab()
+						.SetLabel(LOCTEXT("DetailsTabTitle", "Details"))
+						.SetContent(PropertyView.ToSharedRef())
+					)
+				)
+			)
+		);
+
+	// Initialize the toolkit
+	InitAssetEditor(
+		EToolkitMode::Standalone,
+		nullptr,
+		FName("MyCustomTextureEditor"),
+		StandaloneDefaultLayout,
+		true,
+		true
+	);
+}
---
+++ Source/ProceduralGenerationEditor/ToolKit/BaseEditorToolkit.h
@@ -0,0 +1,43 @@
+﻿// Fill out your copyright notice in the Description page of Project Settings
+#pragma once
+
+#include "CoreMinimal.h"
+#include "Toolkits/AssetEditorToolkit.h"
+
+
+
+class FBaseEditorToolkit : public FAssetEditorToolkit
+{
+
+	// Initialize the asset editor
+	//void Initialize(UProceduralGenerationData* InProceduralAsset, const EToolkitMode::Type Mode, const TSharedPtr<class IToolkitHost>& InitToolkitHost);
+	
+	
+	//OVERRIDES
+	//Register Tab Spawner
+	virtual void RegisterTabSpawners(const TSharedRef<FTabManager>& InTabManager) override;
+	virtual void UnregisterTabSpawners(const TSharedRef<FTabManager>& TabManager) override;
+	virtual FName GetToolkitFName() const override;
+	virtual FText GetBaseToolkitName() const override;
+	virtual FText GetToolkitName() const override;
+	virtual FText GetToolkitToolTipText() const override;
+	virtual FString GetWorldCentricTabPrefix() const override;
+	virtual bool IsAssetEditor() const override;
+	virtual FLinearColor GetWorldCentricTabColorScale() const override;
+	virtual void SaveAsset_Execute() override;
+	virtual void SaveAssetAs_Execute() override;
+	virtual bool OnRequestClose() override;
+	
+	
+	void Init ();
+	
+	
+	private:
+	// Member variables
+	TSharedPtr<class FViewportClient> ViewportClient;
+	TSharedPtr<class IDetailsView> PropertyView;
+	TSharedPtr<class FUICommandList> ToolkitCommands;
+	//UMyCustomTexture* MyCustomTexture;
+	
+};
+	
