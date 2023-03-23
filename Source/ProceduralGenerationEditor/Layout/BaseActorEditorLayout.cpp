62f61b4c68c183f0e7b82dc502949a04060679f9
joydip chakraborty <33289572+XyonX@users.noreply.github.com>
2023-03-23 10:57:56 +0530

    Added Editor Layout Class

---
+++ Source/ProceduralGenerationEditor/Layout/BaseActorEditorLayout.cpp
@@ -0,0 +1,19 @@
+﻿// Fill out your copyright notice in the Description page of Project Settings.
+
+
+#include "BaseActorEditorLayout.h"
+#include "Framework/Docking/TabManager.h"
+
+TSharedRef<FTabManager::FLayout> FBaseActorEditorLayout::EditorLayout()
+{
+	return FTabManager::NewLayout("ProceduralGenerationEditor_Layout_v1")
+			->AddArea(
+				FTabManager::NewPrimaryArea()->SetOrientation(Orient_Vertical)
+				->Split(
+					FTabManager::NewStack()
+					->AddTab(GetTabName(), ETabState::OpenedTab)
+					->SetHideTabWell(true)
+				)
+			);
+	
+}
---
+++ Source/ProceduralGenerationEditor/Layout/BaseActorEditorLayout.h
@@ -0,0 +1,17 @@
+﻿// Fill out your copyright notice in the Description page of Project Settings.
+
+#pragma once
+
+#include "CoreMinimal.h"
+
+
+
+
+class PROCEDURALGENERATIONEDITOR_API FBaseActorEditorLayout
+{
+public:
+	FBaseActorEditorLayout();
+	~FBaseActorEditorLayout();
+
+	TSharedRef<FTabManager::FLayout> EditorLayout ();
+};
