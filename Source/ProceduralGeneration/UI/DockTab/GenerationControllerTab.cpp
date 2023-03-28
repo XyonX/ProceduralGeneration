b6ef574335b6e0c78710cdc04cd9082eb6a144d1
joydip chakraborty <33289572+XyonX@users.noreply.github.com>
2023-03-29 00:10:01 +0530

    Added Generation Controller UI

---
+++ Source/ProceduralGeneration/UI/DockTab/GenerationControllerTab.cpp
@@ -0,0 +1,55 @@
+﻿// Fill out your copyright notice in the Description page of Project Settings.
+
+
+#include "GenerationControllerTab.h"
+#include "EditorStyleSet.h"
+#include "Internationalization//Internationalization.h"
+#include "WorkspaceMenuStructure/Public/WorkspaceMenuStructure.h"
+#include "WorkspaceMenuStructure/Public/WorkspaceMenuStructureModule.h"
+
+
+#define LOCTEXT_NAMESPACE "ControllerText"
+
+const FName SGenerationControllerTab::TabName(TEXT("ControllerTab"));
+
+void SGenerationControllerTab::Construct(const FArguments& InArgs)
+{
+	// Create an instance of your widget
+	Widget = SNew(SVerticalBox);
+
+	// Add the widget to a SBox so we can control the size of the widget
+	TSharedPtr<SBox> MyBox = SNew(SBox)
+		.WidthOverride(100.0f)
+		.HeightOverride(500.0f)
+		[
+			Widget.ToSharedRef()
+		];
+
+	// Set the content of the dockable tab to the SBox containing the widget
+	this->SetContent(MyBox.ToSharedRef());
+}
+
+void SGenerationControllerTab::RegisterTabSpawner()
+{
+	FGlobalTabmanager::Get()->RegisterNomadTabSpawner(
+		TabName,
+		FOnSpawnTab::CreateStatic(&SGenerationControllerTab::SpawnTab))
+		.SetIcon(FSlateIcon(FAppStyle::GetAppStyleSetName(), "LevelEditor.Tabs.Details"))
+		.SetTooltipText(LOCTEXT("MyTabTooltip", "Open MyTab"))
+		.SetGroup(WorkspaceMenu::GetMenuStructure().GetDeveloperToolsMiscCategory())
+		.SetDisplayName(LOCTEXT("Collapsible Box Tab", "CBOX"));
+	
+}
+
+TSharedRef<SDockTab> SGenerationControllerTab::SpawnTab(const FSpawnTabArgs& SpawnTabArgs)
+{
+	TSharedRef<SGenerationControllerTab> NewTab = SNew(SGenerationControllerTab);
+	return NewTab;
+}
+
+FName SGenerationControllerTab::GetTabIdentifier() const
+{
+	return TabName;
+}
+
+#undef  LOCTEXT_NAMESPACE
---
+++ Source/ProceduralGeneration/UI/DockTab/GenerationControllerTab.h
@@ -0,0 +1,30 @@
+﻿// Fill out your copyright notice in the Description page of Project Settings.
+
+#pragma once
+
+#include "CoreMinimal.h"
+
+class SGenerationControllerTab : public SDockTab
+{
+public:
+	SLATE_BEGIN_ARGS(SGenerationControllerTab) {}
+	SLATE_END_ARGS()
+
+	/** Constructs this widget with InArgs */
+	void Construct(const FArguments& InArgs);
+	// Register the dockable tab
+	static void RegisterTabSpawner();
+	
+	// Create an instance of the dockable tab
+	static TSharedRef<SDockTab> SpawnTab(const FSpawnTabArgs& SpawnTabArgs);
+
+
+	//THIS NAME IS FOR  IDENTIFY THIS ACTS   AS A  IDENTIFIER
+	static const FName TabName;
+	// Get the tab identifier
+	FName GetTabIdentifier() const;
+private:
+	TSharedPtr<SVerticalBox> Widget;
+	
+};
+				
