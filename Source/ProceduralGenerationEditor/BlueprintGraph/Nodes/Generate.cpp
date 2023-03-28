0bc9d77f98d2eb167152444787d5816fb70c3f7e
joydip chakraborty <33289572+XyonX@users.noreply.github.com>
2023-03-28 15:16:24 +0530

    Added Custom Generate   EV Node

---
+++ Source/ProceduralGenerationEditor/BlueprintGraph/Nodes/Generate.cpp
@@ -0,0 +1,37 @@
+﻿// Fill out your copyright notice in the Description page of Project Settings.
+
+
+#include "Generate.h"
+
+FText UGenerate::GetNodeTitle(ENodeTitleType::Type TitleType) const
+{
+	return FText::FromString(TEXT("My Custom Node"));
+}
+
+void UGenerate::AllocateDefaultPins()
+{
+	// Create an input pin for the node
+	UEdGraphPin* ExecPin = CreatePin(EGPD_Input, TEXT("ExecPin"), TEXT("ABADABA"), nullptr, false, false, TEXT("Exec"));
+}
+
+FLinearColor UGenerate::GetNodeTitleColor() const
+{
+	return FLinearColor(0, 0, 1);
+}
+
+void UGenerate::NodeConnectionListChanged()
+{
+	Super::NodeConnectionListChanged();
+
+	// Update the input pin on the node
+	UEdGraphPin* ExecPin = FindPin(TEXT("ExecPin"));
+	/*
+	if (ExecPin != nullptr)
+	{
+		ExecPin->bHidden = !ExecPin->LinkedTo.Num() > 0;
+	}*/
+}
+
+void UGenerate::MyCustomFunction()
+{
+}
---
+++ Source/ProceduralGenerationEditor/BlueprintGraph/Nodes/Generate.h
@@ -0,0 +1,21 @@
+﻿// Fill out your copyright notice in the Description page of Project Settings.
+
+#pragma once
+
+#include "CoreMinimal.h"
+#include "Generate.Generated.h"
+
+UCLASS()
+class UGenerate : public UEdGraphNode
+{
+	GENERATED_BODY()
+
+public:
+	virtual FText GetNodeTitle(ENodeTitleType::Type TitleType) const override;
+	virtual void AllocateDefaultPins() override;
+	virtual FLinearColor GetNodeTitleColor() const override;
+	virtual void NodeConnectionListChanged() override;
+
+	UFUNCTION(BlueprintCallable, Category = "Custom Node")
+	void MyCustomFunction();
+};
