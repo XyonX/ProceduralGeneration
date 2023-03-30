d0e3c2c72cd6c823c7ee0901917473a05ba0f732
joydip chakraborty <33289572+XyonX@users.noreply.github.com>
2023-03-30 22:23:27 +0530

    new tab class

---
+++ Source/ProceduralGeneration/UI/DockTab/SControllerTab.h
@@ -0,0 +1,22 @@
+﻿// Fill out your copyright notice in the Description page of Project Settings.
+
+#pragma once
+
+#include "CoreMinimal.h"
+#include "Widgets/SCompoundWidget.h"
+
+/**
+ * 
+ */
+class PROCEDURALGENERATION_API SControllerTab : public SCompoundWidget
+{
+public:
+	SLATE_BEGIN_ARGS(SControllerTab)
+		{
+		}
+
+	SLATE_END_ARGS()
+
+	/** Constructs this widget with InArgs */
+	void Construct(const FArguments& InArgs);
+};
