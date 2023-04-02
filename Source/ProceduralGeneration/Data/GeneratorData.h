7053f6b7256a50252b5e63a2e3d02593685dc358
joydip chakraborty <33289572+XyonX@users.noreply.github.com>
2023-04-02 23:45:12 +0530

    Added Data Asset class

---
+++ Source/ProceduralGeneration/Data/GeneratorData.h
@@ -0,0 +1,17 @@
+﻿#pragma once
+
+#include "CoreMinimal.h"
+#include "GeneratorData.generated.h"
+
+UCLASS(BlueprintType)
+class PROCEDURALGENERATION_API UGeneratorData : public UDataAsset
+{
+	GENERATED_BODY()
+
+public:
+	UPROPERTY(EditAnywhere, BlueprintReadWrite, Category = "Procedural Generation")
+	TArray<FVector> Vertices;
+
+	UPROPERTY(EditAnywhere, BlueprintReadWrite, Category = "Procedural Generation")
+	TArray<int32> Indices;
+};
