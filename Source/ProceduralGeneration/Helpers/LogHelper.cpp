f6ad59e28af485905c48205cda2b32abeb61dfdd
joydip chakraborty <33289572+XyonX@users.noreply.github.com>
2023-04-04 21:08:19 +0530

    Added Log Helper Class

---
+++ Source/ProceduralGeneration/Helpers/LogHelper.cpp
@@ -0,0 +1,13 @@
+﻿// Fill out your copyright notice in the Description page of Project Settings.
+
+
+#include "LogHelper.h"
+
+
+LogHelper::LogHelper()
+{
+}
+
+LogHelper::~LogHelper()
+{
+}
---
+++ Source/ProceduralGeneration/Helpers/LogHelper.h
@@ -0,0 +1,15 @@
+﻿// Fill out your copyright notice in the Description page of Project Settings.
+
+#pragma once
+
+#include "CoreMinimal.h"
+
+#define LogSwitch 0 
+
+
+class PROCEDURALGENERATION_API LogHelper
+{
+public:
+	LogHelper();
+	~LogHelper();
+};
