7ee08338ba72d72c9244a77c0807ad2ded068aca
joydip chakraborty <33289572+XyonX@users.noreply.github.com>
2023-04-06 11:42:30 +0530

    Added Grid Mat Function

---
+++ Source/ProceduralGeneration/Materials/Functions/MF_Grid.cpp
@@ -0,0 +1,13 @@
+﻿// Fill out your copyright notice in the Description page of Project Settings.
+
+
+#include "MF_Grid.h"
+
+
+MF_Grid::MF_Grid()
+{
+}
+
+MF_Grid::~MF_Grid()
+{
+}
---
+++ Source/ProceduralGeneration/Materials/Functions/MF_Grid.h
@@ -0,0 +1,18 @@
+﻿// Fill out your copyright notice in the Description page of Project Settings.
+
+#pragma once
+
+#include "CoreMinimal.h"
+#include "MF_Grid.generated.h"
+
+UCLASS()
+class PROCEDURALGENERATION_API UMF_Grid :  public UMaterialFunctionInterface 
+{
+	GENERATED_BODY()
+public:
+
+	
+
+	
+	
+};
