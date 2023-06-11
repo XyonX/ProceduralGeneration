e80c516bed7323bc1792c031ec3fbf23cf982f9b
joydip chakraborty <33289572+XyonX@users.noreply.github.com>
2023-06-11 22:02:01 +0530

    Added Procedural Gird Code
    This is the actor responsible for generating the grid

---
+++ Source/ProceduralGeneration/Actors/BaseGridActor.cpp
@@ -0,0 +1,114 @@
+﻿// Fill out your copyright notice in the Description page of Project Settings.
+
+
+#include "BaseGridActor.h"
+
+#include "ProceduralMeshComponent.h"
+#include "Materials/MaterialInstanceDynamic.h"
+
+
+struct FProcMeshTangent;
+// Sets default values
+ABaseGridActor::ABaseGridActor()
+{
+	// Set this actor to call Tick() every frame.  You can turn this off to improve performance if you don't need it.
+	PrimaryActorTick.bCanEverTick = true;
+
+	GridMesh=CreateDefaultSubobject<UProceduralMeshComponent>("Grid Preocedural Mesh");
+	NumColumns=16;
+	NumRows=16;
+	CellSize=4;
+}
+
+// Called when the game starts or when spawned
+void ABaseGridActor::BeginPlay()
+{
+	Super::BeginPlay();
+	GenerateGridMesh();
+	
+}
+
+// Called every frame
+void ABaseGridActor::Tick(float DeltaTime)
+{
+	Super::Tick(DeltaTime);
+}
+
+bool ABaseGridActor::GenerateGridMesh()
+{
+	// Calculate the grid size
+	const float HalfWidth = NumColumns * CellSize * 0.5f;
+	const float HalfHeight = NumRows * CellSize * 0.5f;
+
+	for (int32 Row = 0; Row <= NumRows; ++Row)
+	{
+		for (int32 Col = 0; Col <= NumColumns; ++Col)
+		{
+			const FVector VertexLocation = FVector(Col * CellSize - HalfWidth, Row * CellSize - HalfHeight, 0.0f);
+
+			// Add vertex
+			Vertices.Add(VertexLocation);
+
+			// Add UVs
+			const FVector2D UV((float)Col / NumColumns, (float)Row / NumRows);
+			UVs.Add(UV);
+
+			// Skip the last row and column as they won't form triangles
+			if (Row < NumRows && Col < NumColumns)
+			{
+				const int32 VertexIndex = Row * (NumColumns + 1) + Col;
+
+				// Create triangles
+				Triangles.Add(VertexIndex);
+				Triangles.Add(VertexIndex + 1);
+				Triangles.Add(VertexIndex + NumColumns + 1);
+
+				Triangles.Add(VertexIndex + NumColumns + 1);
+				Triangles.Add(VertexIndex + 1);
+				Triangles.Add(VertexIndex + NumColumns + 2);
+
+				// Add separate materials for each tile
+				//UMaterialInstanceDynamic* DynMaterial = UMaterialInstanceDynamic::Create(MaterialTemplate, this);
+
+				// Set the desired color for the tile
+				//FLinearColor ColorParameter = GetTileColor(Row, Col); // Implement this function in your class to return the desired color for the tile
+
+				/*float RandomFloat = FMath::RandRange(0,1);
+				FLinearColor ColorParameter =FLinearColor(RandomFloat,1.0,1.0,1.0f);
+				DynMaterial->SetVectorParameterValue("BaseColor", ColorParameter);
+				GridMesh->SetMaterial(VertexIndex, DynMaterial);*/
+				// Update the material color
+				
+
+				// Create a dynamic material instance for each tile
+				UMaterialInstanceDynamic* DynMaterial = UMaterialInstanceDynamic::Create(MaterialTemplate, this);
+
+				// Set the desired color for the tile
+				FLinearColor ColorParameter = FLinearColor::MakeRandomColor();
+				DynMaterial->SetVectorParameterValue("BaseColor", ColorParameter);
+
+				GridMesh->SetMaterial(VertexIndex, DynMaterial);
+			}
+		}
+	}
+
+	// Calculate normals (assuming flat grid)
+	for (int32 i = 0; i < Vertices.Num(); ++i)
+	{
+		Normals.Add(FVector(0.0f, 0.0f, -1.0f));
+	}
+
+	// Update the grid mesh
+	GridMesh->CreateMeshSection(0, Vertices, Triangles, Normals, UVs, TArray<FColor>(), TArray<FProcMeshTangent>(), false);
+	/*
+	// Update the grid mesh
+	GridMesh->CreateMeshSection(0, Vertices, Triangles, Normals, UVs, TArray<FColor>(), TArray<FProcMeshTangent>(), false);
+	UMaterialInstanceDynamic* DynMaterial = UMaterialInstanceDynamic::Create(MaterialTemplate, this);
+	FLinearColor ColorParameter = FLinearColor::Black; // Set the desired color here
+	DynMaterial->SetVectorParameterValue("BaseColor", ColorParameter);
+	GridMesh->SetMaterial(0, DynMaterial);*/
+
+
+	return true;
+}
+
---
+++ Source/ProceduralGeneration/Actors/BaseGridActor.h
@@ -0,0 +1,51 @@
+﻿// Fill out your copyright notice in the Description page of Project Settings.
+
+#pragma once
+
+#include "CoreMinimal.h"
+#include "GameFramework/Actor.h"
+#include "BaseGridActor.generated.h"
+
+class UProceduralMeshComponent;
+UCLASS(BlueprintType)
+class PROCEDURALGENERATION_API ABaseGridActor : public AActor
+{
+	GENERATED_BODY()
+
+public:
+	// Sets default values for this actor's properties
+	ABaseGridActor();
+
+protected:
+	// Called when the game starts or when spawned
+	virtual void BeginPlay() override;
+
+public:
+	// Called every frame
+	virtual void Tick(float DeltaTime) override;
+
+	UPROPERTY(EditAnywhere,BlueprintReadWrite,Category="Mat")
+	UMaterialInterface* MaterialTemplate;
+
+private:
+
+	UPROPERTY(BlueprintReadWrite,EditAnywhere,Category="ProcMesh",meta=(AllowPrivateAccess=True))
+	UProceduralMeshComponent* GridMesh;
+
+	UPROPERTY(BlueprintReadWrite,EditAnywhere,Category="ProcMesh",meta=(AllowPrivateAccess=True))
+	int32 NumRows;
+	UPROPERTY(BlueprintReadWrite,EditAnywhere,Category="ProcMesh",meta=(AllowPrivateAccess=True))
+	int32 NumColumns;
+	UPROPERTY(BlueprintReadWrite,EditAnywhere,Category="ProcMesh",meta=(AllowPrivateAccess=True))
+	float CellSize;
+
+	TArray<FVector>Vertices;
+	TArray<int32>Triangles;
+	TArray<FVector>Normals;
+	TArray<FVector2D>UVs;
+
+public:
+	// Generate the grid mesh
+	bool GenerateGridMesh();
+	
+};
