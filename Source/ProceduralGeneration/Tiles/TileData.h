77666e090f2560bd53bd691f1fa697793d47c42e
joydip chakraborty <33289572+XyonX@users.noreply.github.com>
2023-06-13 11:00:28 +0530

    Added Tile Data Class

---
+++ Source/ProceduralGeneration/Tiles/TileData.h
@@ -0,0 +1,22 @@
+﻿// Fill out your copyright notice in the Description page of Project Settings.
+
+#pragma once
+
+#include "CoreMinimal.h"
+#include "UObject/Object.h"
+#include "TileData.generated.h"
+
+/**
+ * 
+ */
+UCLASS()
+class PROCEDURALGENERATION_API UTileData : public UObject
+{
+	GENERATED_BODY()
+public:
+
+	UPROPERTY(EditAnywhere,BlueprintReadWrite)
+	int ID ;
+	//UPROPERTY(EditAnywhere,BlueprintReadWrite)
+	//FVector2d Pos ;
+};
