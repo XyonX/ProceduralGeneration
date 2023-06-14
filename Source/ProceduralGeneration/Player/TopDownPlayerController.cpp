d723f729228cd0408b0493afc1a633c5ad21d7a6
joydip chakraborty <33289572+XyonX@users.noreply.github.com>
2023-06-14 11:48:28 +0530

    Added TopDown Controller

---
+++ Source/ProceduralGeneration/Player/TopDownPlayerController.cpp
@@ -0,0 +1,62 @@
+﻿// Fill out your copyright notice in the Description page of Project Settings.
+
+
+#include "TopDownPlayerController.h"
+
+#include "EnhancedInputComponent.h"
+#include "GameFramework/Character.h"
+#include "ProceduralGeneration/Actors/BaseGridActor.h"
+
+
+class ABaseGridActor;
+
+void ATopDownPlayerController::OnMouseMoveX(const FInputActionValue& Value)
+{
+	FVector2D MousePosition;
+	GetMousePosition(MousePosition.X, MousePosition.Y);
+	OnMouseMove(MousePosition);
+}
+
+void ATopDownPlayerController::OnMouseMoveY(const FInputActionValue& Value)
+{
+	FVector2D MousePosition;
+	GetMousePosition(MousePosition.X, MousePosition.Y);
+	OnMouseMove(MousePosition);
+}
+
+void ATopDownPlayerController::OnMouseMove(const FVector2D& MousePosition)
+{
+	// Implement the logic for hovering over the grid and updating the mesh here
+	// You can call the corresponding function in your grid actor or widget class
+	if (ABaseGridActor* GridActor = Cast<ABaseGridActor>(GetPawn()))
+	{
+		GridActor->OnMouseMove(MousePosition);
+	}
+
+}
+
+
+
+
+
+void ATopDownPlayerController::SetupInputComponent()
+{
+	Super::SetupInputComponent();
+	UEnhancedInputComponent* EnhancedInputComp =Cast <UEnhancedInputComponent>(InputComponent);
+
+	if(EnhancedInputComp)
+	{
+
+		if(MouseX)
+		{
+			EnhancedInputComp->BindAction(MouseX,ETriggerEvent::Triggered,this,&ATopDownPlayerController::OnMouseMoveX);
+		}
+		if(MouseY)
+		{
+			EnhancedInputComp->BindAction(MouseY,ETriggerEvent::Triggered,this,&ATopDownPlayerController::OnMouseMoveY);
+		}
+	}
+	
+}
+
+
---
+++ Source/ProceduralGeneration/Player/TopDownPlayerController.h
@@ -0,0 +1,30 @@
+﻿// Fill out your copyright notice in the Description page of Project Settings.
+
+#pragma once
+
+#include "CoreMinimal.h"
+#include "InputActionValue.h"
+#include"GameFramework/PlayerController.h"
+#include "TopDownPlayerController.generated.h"
+
+
+UCLASS()
+class PROCEDURALGENERATION_API ATopDownPlayerController  : public APlayerController
+{
+	GENERATED_BODY()
+
+public:
+	virtual void SetupInputComponent() override;
+
+
+	void OnMouseMoveX (const FInputActionValue& Value);
+	void OnMouseMoveY (const FInputActionValue& Value);
+
+	void OnMouseMove(const FVector2D& MousePosition);
+
+	UPROPERTY(EditDefaultsOnly,BlueprintReadWrite,Category="Input Actions")
+	class UInputAction* MouseX ;
+	UPROPERTY(EditDefaultsOnly,BlueprintReadWrite,Category="Input Actions")
+	class UInputAction* MouseY ;
+	
+};
