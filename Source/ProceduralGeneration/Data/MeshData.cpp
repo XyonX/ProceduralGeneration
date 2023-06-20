c8da1a5b24c4527fd0e5ffe8b72cd2abcf046454
joydip chakraborty <33289572+XyonX@users.noreply.github.com>
2023-06-21 01:22:17 +0530

    Added MeshData Data Asset Class

---
+++ Source/ProceduralGeneration/Data/MeshData.cpp
@@ -0,0 +1,1 @@
+﻿#include "MeshData.h"
---
+++ Source/ProceduralGeneration/Data/MeshData.h
@@ -0,0 +1,36 @@
+﻿#pragma once
+
+#include "CoreMinimal.h"
+#include "MeshData.generated.h"
+
+UCLASS(BlueprintType)
+class PROCEDURALGENERATION_API UMeshData : public UDataAsset
+{
+	GENERATED_BODY()
+
+public:
+	//UPROPERTY(EditAnywhere, BlueprintReadWrite, Category = "Procedural Generation")
+	//TArray<FVector> Vertices;
+
+	//UPROPERTY(EditAnywhere, BlueprintReadWrite, Category = "Procedural Generation")
+	//TArray<int32> Indices;
+	UPROPERTY(EditAnywhere,BlueprintReadWrite,Category="Data")
+	TArray<UStaticMesh*>PrefabBuildings;
+	UPROPERTY(EditAnywhere,BlueprintReadWrite,Category="Data")
+	TArray<UStaticMesh*>Walls;
+	UPROPERTY(EditAnywhere,BlueprintReadWrite,Category="Data")
+	TArray<UStaticMesh*>Roads1;
+	UPROPERTY(EditAnywhere,BlueprintReadWrite,Category="Data")
+	TArray<UStaticMesh*>Roads2;
+	UPROPERTY(EditAnywhere,BlueprintReadWrite,Category="Data")
+	TArray<UStaticMesh*>Roads3;
+	UPROPERTY(EditAnywhere,BlueprintReadWrite,Category="Data")
+	TArray<UStaticMesh*>RoadJunction;
+	UPROPERTY(EditAnywhere,BlueprintReadWrite,Category="Data")
+	TArray<UStaticMesh*>SideWalk1;
+	UPROPERTY(EditAnywhere,BlueprintReadWrite,Category="Data")
+	TArray<UStaticMesh*>SideWalk2;
+	UPROPERTY(EditAnywhere,BlueprintReadWrite,Category="Data")
+	TArray<UStaticMesh*>SideWalk3;
+	
+};
