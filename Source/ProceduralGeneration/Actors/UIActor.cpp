653d966eef645566457eb454e844c44e12678675
joydip chakraborty <33289572+XyonX@users.noreply.github.com>
2023-06-20 19:38:56 +0530

    Added  UI Actor to Manage UI From CODE

---
+++ Source/ProceduralGeneration/Actors/UIActor.cpp
@@ -0,0 +1,34 @@
+﻿// Fill out your copyright notice in the Description page of Project Settings.
+
+
+#include "UIActor.h"
+
+#include "Actions/AsyncAction_CreateWidgetAsync.h"
+#include "CoreUI/UI/TopDownIngameScreen.h"
+
+
+// Sets default values
+AUIActor::AUIActor()
+{
+	// Set this actor to call Tick() every frame.  You can turn this off to improve performance if you don't need it.
+	PrimaryActorTick.bCanEverTick = true;
+}
+
+// Called when the game starts or when spawned
+void AUIActor::BeginPlay()
+{
+	Super::BeginPlay();
+	// Spawn the UTopDownIngameScreen widget
+	 IngameScreen = CreateWidget<UTopDownIngameScreen>(GetWorld(), UTopDownIngameScreen::StaticClass());
+	IngameScreen->SizeBoxSize=Size;
+	IngameScreen->BGImage=BGImage;
+	IngameScreen->AddToViewport();
+	
+}
+
+// Called every frame
+void AUIActor::Tick(float DeltaTime)
+{
+	Super::Tick(DeltaTime);
+}
+
---
+++ Source/ProceduralGeneration/Actors/UIActor.h
@@ -0,0 +1,38 @@
+﻿// Fill out your copyright notice in the Description page of Project Settings.
+
+#pragma once
+
+#include "CoreMinimal.h"
+#include "CoreUI/UI/TopDownIngameScreen.h"
+#include "GameFramework/Actor.h"
+#include "UIActor.generated.h"
+
+UCLASS()
+class PROCEDURALGENERATION_API AUIActor : public AActor
+{
+	GENERATED_BODY()
+
+public:
+	// Sets default values for this actor's properties
+	AUIActor();
+
+protected:
+	// Called when the game starts or when spawned
+	virtual void BeginPlay() override;
+
+public:
+	// Called every frame
+	virtual void Tick(float DeltaTime) override;
+
+	UPROPERTY(EditAnywhere,BlueprintReadWrite,Category="UI")
+	FVector2D Size;
+	UPROPERTY(EditAnywhere,BlueprintReadWrite,Category="UI")
+	UTexture2D* BGImage;
+
+	UFUNCTION(BlueprintCallable)
+	UTopDownIngameScreen*GetIngameWidget () { return IngameScreen;}
+private:
+
+	
+	UTopDownIngameScreen*IngameScreen ;
+};
