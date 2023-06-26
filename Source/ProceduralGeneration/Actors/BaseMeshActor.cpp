c62a95ecb957cffb065b7bdca3c6d067a4f64fa5
joydip chakraborty <33289572+XyonX@users.noreply.github.com>
2023-06-26 23:58:23 +0530

    Added Actor Class  for buildings

---
+++ Source/ProceduralGeneration/Actors/BaseMeshActor.cpp
@@ -0,0 +1,32 @@
+﻿// Fill out your copyright notice in the Description page of Project Settings.
+
+
+#include "BaseMeshActor.h"
+
+
+// Sets default values
+ABaseMeshActor::ABaseMeshActor()
+{
+	PrimaryActorTick.bCanEverTick = false;
+	MeshCompoonent= CreateDefaultSubobject<UStaticMeshComponent>("MeshComponent");
+	SetRootComponent(MeshCompoonent);
+}
+
+// Called when the game starts or when spawned
+void ABaseMeshActor::BeginPlay()
+{
+	Super::BeginPlay();
+	
+}
+
+void ABaseMeshActor::Init(UStaticMesh*inMesh,UMaterialInstanceDynamic*inMaterial)
+{
+	Mesh=inMesh;
+	Material=inMaterial;
+	
+	if(MeshCompoonent)
+	{
+		MeshCompoonent->SetStaticMesh(Mesh);
+		MeshCompoonent->CreateAndSetMaterialInstanceDynamic(0);
+	}
+}
---
+++ Source/ProceduralGeneration/Actors/BaseMeshActor.h
@@ -0,0 +1,37 @@
+﻿// Fill out your copyright notice in the Description page of Project Settings.
+
+#pragma once
+
+#include "CoreMinimal.h"
+#include "GameFramework/Actor.h"
+#include "Materials/MaterialInstance.h"
+#include "BaseMeshActor.generated.h"
+
+UCLASS()
+class PROCEDURALGENERATION_API ABaseMeshActor : public AActor
+{
+	GENERATED_BODY()
+
+public:
+	// Sets default values for this actor's properties
+	ABaseMeshActor();
+
+protected:
+	// Called when the game starts or when spawned
+	virtual void BeginPlay() override;
+
+	UPROPERTY(EditAnywhere,BlueprintReadWrite,Category="MeshActor")
+	UStaticMeshComponent*MeshCompoonent;
+
+	UPROPERTY(EditAnywhere,BlueprintReadWrite,Category="MeshActor")
+	UStaticMesh*Mesh;
+	UPROPERTY(EditAnywhere,BlueprintReadWrite,Category="MeshActor")
+	UMaterialInstanceDynamic*Material;
+
+public:
+	
+	void SetMesh(UStaticMesh*inMesh) {Mesh=inMesh;}
+	UStaticMesh* GetMesh () {return  Mesh;};
+
+	void Init (UStaticMesh*inMesh,UMaterialInstanceDynamic*inMaterial);
+};
