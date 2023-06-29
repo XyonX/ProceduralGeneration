af99ba312f5a970ef428bb1d1d561ea92d612fd6
joydip chakraborty <33289572+XyonX@users.noreply.github.com>
2023-06-29 11:52:08 +0530

    Added Game Instance class

---
+++ Source/ProceduralGeneration/Game/TopDownGameInstance.cpp
@@ -0,0 +1,87 @@
+﻿// Fill out your copyright notice in the Description page of Project Settings.
+
+
+#include "TopDownGameInstance.h"
+
+#include "CorePlugin/Data/MeshData.h"
+#include "CorePlugin/Spawnables/Spawnable.h"
+#include "Engine/Texture2DArray.h"
+#include "ThumbnailRendering/ThumbnailManager.h"
+#include "UObject/ConstructorHelpers.h"
+
+UTopDownGameInstance::UTopDownGameInstance(const FObjectInitializer& ObjectInitializer)
+	:Super(ObjectInitializer)
+{
+}
+/*
+UDataTable* UTopDownGameInstance::ImportData(const FString& DataTablePath)
+{
+//return   Cast<UDataTable>(StaticLoadObject(UDataTable::StaticClass(), nullptr, *DataTablePath));
+
+	FString AssetPath = FString::Printf(TEXT("/ProceduralGeneration/Data/%s.%s"), *SpawnableItemsDataTableName, *SpawnableItemsDataTableName);
+	//FString AssetPath = FString::Printf(TEXT("ProceduralGeneration/Data/DT_MeshProperty.DT_MeshProperty"));
+	//return   Cast<UDataTable>(StaticLoadObject(UDataTable::StaticClass(), nullptr, *DataTablePath));
+	return   Cast<UDataTable>(StaticLoadObject(UDataTable::StaticClass(), nullptr, *AssetPath));
+}*/
+
+UDataTable* UTopDownGameInstance::ImportData(FString DataTableName)
+{
+	FString AssetPath = FString::Printf(TEXT("/ProceduralGeneration/Data/%s.%s"), *DataTableName, *DataTableName);
+	return   Cast<UDataTable>(StaticLoadObject(UDataTable::StaticClass(), nullptr, *AssetPath));
+}
+
+void UTopDownGameInstance::Init_Spawnable(UDataTable* inDT, TArray<USpawnable*>& inSpawnables)
+{
+	TArray<FMeshProperty*>AllMeshProperties;
+	inDT->GetAllRows<FMeshProperty>(TEXT(""),AllMeshProperties);
+
+	if (AllMeshProperties.IsEmpty())
+	{
+		return;
+	}
+	//UThumbnailManager& ThumbnailManager = UThumbnailManager::Get();
+	
+	for (FMeshProperty*MP : AllMeshProperties)
+	{
+		USpawnable * Spawnable = NewObject<USpawnable>();
+		Spawnable->Init(MP->Mesh,MP->Icon,MP->TilingType,MP->MeshPivotPosition,MP->MeshQuadrantPosition);
+		inSpawnables.Add(Spawnable);
+	}
+	
+
+	
+}
+
+void UTopDownGameInstance::Init_Spawnable(UDataTable* inDT, TMap<int32, USpawnable*>& inSpawnables)
+{
+	TArray<FMeshProperty*>AllMeshProperties;
+	inDT->GetAllRows<FMeshProperty>(TEXT(""),AllMeshProperties);
+
+	if (AllMeshProperties.IsEmpty())
+	{
+		return;
+	}
+	//UThumbnailManager& ThumbnailManager = UThumbnailManager::Get();
+	
+	for (FMeshProperty*MP : AllMeshProperties)
+	{
+		USpawnable * Spawnable = NewObject<USpawnable>();
+		Spawnable->Init(MP->Mesh,MP->Icon,MP->TilingType,MP->MeshPivotPosition,MP->MeshQuadrantPosition);
+		inSpawnables.Add(Spawnable->GetID(),Spawnable);
+	}
+}
+
+void UTopDownGameInstance::Init()
+{
+	Super::Init();
+
+	// Usage example:
+	FString DataTablePath = "/ProceduralGeneration/Data/DT_MeshProperty.DT_MeshProperty";
+	UDataTable* LoadedDataTable =ImportData(SpawnableItemsDataTableName);
+	if (LoadedDataTable)
+	{
+		if(GEngine){GEngine->AddOnScreenDebugMessage(-1, 2.0f, FColor::Red, TEXT(" Data Table Found "));}
+	}
+	
+	Init_Spawnable(LoadedDataTable,SpawnableItems);
+}
---
+++ Source/ProceduralGeneration/Game/TopDownGameInstance.h
@@ -0,0 +1,36 @@
+﻿#pragma once
+
+#include "CoreMinimal.h"
+#include "Engine/GameInstance.h"
+#include "TopDownGameInstance.generated.h"
+
+class USpawnable;
+UCLASS()
+class PROCEDURALGENERATION_API UTopDownGameInstance : public UGameInstance
+{
+	GENERATED_BODY()
+
+public:
+	UTopDownGameInstance(const FObjectInitializer& ObjectInitializer);
+
+
+	UPROPERTY(EditAnywhere,BlueprintReadWrite,Category="GI")
+	FString SpawnableItemsDataTableName ;
+	
+	// Data Table storing the static meshes and its properties
+	UPROPERTY(EditAnywhere,BlueprintReadWrite,Category="GI")
+	UDataTable*SpawnableItemsData;
+
+	//Processed Imported Data
+	//UPROPERTY(EditAnywhere,BlueprintReadWrite,Category="GI")
+	//TArray<USpawnable*>SpawnableItems;
+	UPROPERTY(EditAnywhere,BlueprintReadWrite,Category="GI")
+	TMap<int32 ,USpawnable*>SpawnableItems;
+
+	UDataTable* ImportData (FString DataTableName);
+
+	void Init_Spawnable (UDataTable*inDT,TArray<USpawnable*>&inSpawnables);
+	void Init_Spawnable (UDataTable*inDT,TMap<int32 ,USpawnable*>&inSpawnables);
+	
+	virtual void Init() override;
+};
