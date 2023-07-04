b04f50fee4c5f2fb4dea08df92d73019a0d07b11
joydip chakraborty <33289572+XyonX@users.noreply.github.com>
2023-07-04 12:54:48 +0530

    Added Top Down GameMode

---
+++ Source/ProceduralGeneration/Game/TopDownGameMode.cpp
@@ -0,0 +1,12 @@
+﻿#include "TopDownGameMode.h"
+
+#include "ProceduralGeneration/Pawn/TopDownPawn.h"
+#include "ProceduralGeneration/Player/TopDownPlayerController.h"
+
+ATopDownGameMode::ATopDownGameMode(const FObjectInitializer& ObjectInitializer)
+	:Super(ObjectInitializer)
+{
+	
+	DefaultPawnClass=ATopDownPawn::StaticClass();
+	PlayerControllerClass=ATopDownPlayerController::StaticClass();
+}
---
+++ Source/ProceduralGeneration/Game/TopDownGameMode.h
@@ -0,0 +1,14 @@
+﻿#pragma once
+
+#include "GameFramework/GameMode.h"
+#include "TopDownGameMode.generated.h"
+
+
+UCLASS(Blueprintable)
+class PROCEDURALGENERATION_API ATopDownGameMode : public AGameModeBase
+{
+	GENERATED_BODY()
+
+public:
+	ATopDownGameMode(const FObjectInitializer& ObjectInitializer = FObjectInitializer::Get());
+};
