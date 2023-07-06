d4410107fe676a1b6b6baa0e1451bf39a8bb7374
joydip chakraborty <33289572+XyonX@users.noreply.github.com>
2023-07-06 22:45:35 +0530

    Added Runtime Character Appearance Editor Class

---
+++ Source/ProceduralGeneration/Actors/RuntimeAppearanceEditor.cpp
@@ -0,0 +1,39 @@
+﻿// Fill out your copyright notice in the Description page of Project Settings.
+
+
+#include "RuntimeAppearanceEditor.h"
+#include "KismetProceduralMeshLibrary.h"
+
+
+// Sets default values
+ARuntimeAppearanceEditor::ARuntimeAppearanceEditor()
+{
+	// Set this actor to call Tick() every frame.  You can turn this off to improve performance if you don't need it.
+	PrimaryActorTick.bCanEverTick = true;
+}
+
+// Called when the game starts or when spawned
+void ARuntimeAppearanceEditor::BeginPlay()
+{
+	Super::BeginPlay();
+	
+}
+
+// Called every frame
+void ARuntimeAppearanceEditor::Tick(float DeltaTime)
+{
+	Super::Tick(DeltaTime);
+}
+
+void ARuntimeAppearanceEditor::ExtractMeshData()
+{
+	UKismetProceduralMeshLibrary::CopyProceduralMeshFromStaticMeshComponent(CharacterMeshComponent,0,PMC,false);
+
+	 PMS_FullBody= PMC->GetProcMeshSection(0);
+
+	UKismetProceduralMeshLibrary::GetSectionFromStaticMesh(CharacterMesh,0,Index,Vertices,Triangles,Normals,UVs,Tangents);
+
+	
+	PMC->GetProcMeshSection(0);
+}
+
---
+++ Source/ProceduralGeneration/Actors/RuntimeAppearanceEditor.h
@@ -0,0 +1,49 @@
+﻿// Fill out your copyright notice in the Description page of Project Settings.
+
+#pragma once
+
+#include "CoreMinimal.h"
+#include "ProceduralMeshComponent.h"
+#include "GameFramework/Actor.h"
+#include "RuntimeAppearanceEditor.generated.h"
+
+
+/** Class to Modify Character Mesh Appearance at Runtime */
+UCLASS()
+class PROCEDURALGENERATION_API ARuntimeAppearanceEditor : public AActor
+{
+	GENERATED_BODY()
+
+public:
+	// Sets default values for this actor's properties
+	ARuntimeAppearanceEditor();
+
+protected:
+	// Called when the game starts or when spawned
+	virtual void BeginPlay() override;
+
+	UPROPERTY()
+	UStaticMesh*CharacterMesh;
+	UPROPERTY()
+	UStaticMeshComponent* CharacterMeshComponent;
+	UPROPERTY()
+	UProceduralMeshComponent*PMC;
+
+public:
+	// Called every frame
+	virtual void Tick(float DeltaTime) override;
+
+
+	void ExtractMeshData ();
+
+private:
+
+	int32 Index;
+	TArray<FVector> Vertices;
+	TArray<int32> Triangles;
+	TArray<FVector> Normals;
+	TArray<FVector2D> UVs;
+	TArray<FProcMeshTangent> Tangents;
+
+	FProcMeshSection*PMS_FullBody;
+};
