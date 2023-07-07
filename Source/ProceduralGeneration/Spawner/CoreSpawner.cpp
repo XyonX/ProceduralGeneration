b8b57d4975aeb55bd36e641b7fd965b3815d79ec
joydip chakraborty <33289572+XyonX@users.noreply.github.com>
2023-07-07 21:44:30 +0530

    Added Core Spawner Class

---
+++ Source/ProceduralGeneration/Spawner/CoreSpawner.cpp
@@ -0,0 +1,16 @@
+﻿// Fill out your copyright notice in the Description page of Project Settings.
+
+
+#include "CoreSpawner.h"
+
+
+
+bool UCoreSpawner::ImportSpawnable(UTopDownGameInstance*InGameInstance , TMap<int32,USpawnable*>* OutSpawnable)
+{
+	if(InGameInstance)
+	{
+		OutSpawnables = InGameInstance->GetAllSpawnables();
+		return true;
+	}
+	return false;
+}
---
+++ Source/ProceduralGeneration/Spawner/CoreSpawner.h
@@ -0,0 +1,39 @@
+﻿// Fill out your copyright notice in the Description page of Project Settings.
+
+#pragma once
+
+#include "CoreMinimal.h"
+#include "GameplayTagContainer.h"
+#include "CorePlugin/Spawnables/Spawnable.h"
+#include "ProceduralGeneration/Game/TopDownGameInstance.h"
+#include "UObject/Object.h"
+#include "CoreSpawner.generated.h"
+
+/**
+ This class  Handel Mesh Spawning Related Logics
+ -TileMesh Replaced With Spawner
+ */
+UCLASS()
+class PROCEDURALGENERATION_API UCoreSpawner : public UObject
+{
+	GENERATED_BODY()
+
+public:
+
+	bool ImportSpawnable (UTopDownGameInstance*InGameInstance , TMap<int32,USpawnable*>* OutSpawnable );
+
+protected:
+
+	
+	
+
+private:
+
+	UPROPERTY()
+	UDataTable*MeshPropertyDataTable;
+	UPROPERTY()
+	TMap<int32,USpawnable*>* TotalSpawnable;
+
+	UPROPERTY()
+	UTopDownGameInstance*TopDownGameInstance;
+};
